6175850f552a77af8956598409b77783fc42388d
Sabbir-Ahamed <sabbir42070@gmail.com>
2022-04-05 05:51:07 +0600

    dashboard done

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { Route, Routes } from "react-router-dom";
 import "./App.css";
 import About from "./Components/About/About";
+import Dashboard from "./Components/Dashboard/Dashboard";
 import Header from "./Components/Header/Header";
 import Home from "./Components/Home/Home";
 import HomeReviews from "./Components/HomeReviews/HomeReviews";
@@ -17,10 +18,9 @@ function App() {
         <Route path="/home" element={<><Home></Home><HomeReviews></HomeReviews></>}></Route>
         <Route path="/reviews" element={<Reviews></Reviews>}></Route>
         <Route path="/about" element={<About></About>}></Route>
+        <Route path="/dashboard" element={<Dashboard></Dashboard>}></Route>
         <Route path="*" element={<NotFound></NotFound>}></Route>
       </Routes>
-      
-      
       
     </div>
   );
--- src/Components/Dashboard/Dashboard.js
+++ src/Components/Dashboard/Dashboard.js
@@ -1,10 +1,65 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { Area, AreaChart, CartesianGrid, Line, LineChart, Tooltip, XAxis, YAxis } from 'recharts';
+import "./Dashboard.css";
 
 const Dashboard = () => {
+
+    const [chartData , setChartData] = useState([]);
+
+    useEffect( () => {
+        fetch("data.json")
+        .then(res => res.json())
+        .then(data => setChartData(data))
+    } , []);
+
+    console.log(chartData);
+
     return (
+      <div className="dashboard">
         <div>
-            ami dash dash
+          <h2>Month wise Sell</h2>
+          <LineChart width={600} height={300} data={chartData}>
+            <Line type="monotone" dataKey="sell" stroke="#8884d8" />
+            <CartesianGrid stroke="#ccc" />
+            <XAxis dataKey="month" />
+            <YAxis />
+            <Tooltip />
+          </LineChart>
         </div>
+        <div>
+          <h2>Month wise Investment and Revinue</h2>
+
+          <AreaChart
+            width={600}
+            height={300}
+            data={chartData}
+            margin={{
+              top: 5,
+              right: 0,
+              left: 0,
+              bottom: 5,
+            }}
+          >
+            <Area
+              type="monotone"
+              dataKey="investment"
+              stroke="#8884d8"
+              fill="#8884d8"
+            />
+            <Area
+              type="monotone"
+              dataKey="revenue"
+              stroke="#65C18C"
+              fill="#65C18C"
+            />
+
+            <CartesianGrid stroke="#ccc" />
+            <XAxis dataKey="month" />
+            <YAxis />
+            <Tooltip />
+          </AreaChart>
+        </div>
+      </div>
     );
 };
 
